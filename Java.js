266698b6d7aefa669f62028278cc56cbd2fc3394
oksherlock <oksherlock9661@gmail.com>
2025-03-07 06:30:45 +0100

    0.2

---
+++ Java.js
@@ -0,0 +1,20 @@
+function bugor(){
+    document.getElementById("sidebar").style.width = "250px";
+
+    document.getElementById("curtain").style.opacity = "0.5";
+
+    document.getElementById("curtain").style.zIndex = "12";
+
+    document.getElementById("close").style.color = "black"
+
+}
+
+function closeh(){
+    document.getElementById("sidebar").style.width = "0";
+
+    document.getElementById("curtain").style.opacity = "0";
+
+    document.getElementById("curtain").style.zIndex = "-1";
+
+    document.getElementById("close").style.color = "white"
+}
